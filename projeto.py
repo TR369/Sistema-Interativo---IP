3426c88e45c5160cc2133908a06f02197d628e23
Filipe Bessa <filipegbessa@gmail.com>
2020-11-30 12:03:14 -0300

    Variables names formatting changes

--- projeto.py
+++ projeto.py
@@ -5,38 +5,35 @@ from score import Score
 from food import Food
 
 pygame.init()
-screen_width = 800
-screen_height = 684
-gameDisplay = pygame.display.set_mode((screen_width, screen_height))
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 684
+gameDisplay = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Castle")
 clock = pygame.time.Clock()
 
 BLACK = (0, 0, 0)
 
-player = Player(BLACK, screen_width, screen_height)
-score = Score(gameDisplay, 0, screen_width - 20, 20)
+player = Player(BLACK, SCREEN_WIDTH, SCREEN_HEIGHT)
+score = Score(gameDisplay, 0, SCREEN_WIDTH - 20, 20)
 hunger = Hunger(gameDisplay)
 
 indoor_hirule = pygame.image.load("hirule.png")
 
-x = screen_width / 2
-y = screen_height / 2
 
-dead = False
+DEAD = False
 
-while not dead:
+while not DEAD:
     dt = clock.tick(60)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            dead = True
+            DEAD = True
 
     gameDisplay.blit(indoor_hirule, (0, 0))
     player.update()
     score.update(0)
     hunger.update(dt)
     pygame.display.update()
-    
 
 
 pygame.quit()
